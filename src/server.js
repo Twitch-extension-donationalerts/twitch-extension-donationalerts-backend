2ee614bc2bf2949459227e28650a12ca62ab31f3
nkroe <nikitak123@mail.ru>
2019-11-13 19:18:05 +0500

    fix

--- src/server.js
+++ src/server.js
@@ -60,39 +60,47 @@ app.get("/getDonations/:user_id", (req, res) => {
   Users.find({
     user_id: req.params.user_id
   }).then(data => {
+    let result = [];
     if (data.length) {
-      axios.get(`${DA_URL}/api/v1/alerts/donations`, {
-        headers: {
-          Authorization: `Bearer ${data[0].accessToken}`
-        }
-      }).then(_data => {
-        let donations = _data.data.data.map(w => {
-          if (w.currency === "RUB") {
-            return {
-              amount: w.amount,
-              username: w.username
-            };
-          } else if (w.currency === "USD") {
-            return {
-              amount: w.amount * 62,
-              username: w.username
-            };
-          } else if (w.currency === "EUR") {
-            return {
-              amount: w.amount * 71,
-              username: w.username
-            };
+      const getDonation = (link) => {
+        console.log(link);
+        axios.get(link, {
+          headers: {
+            Authorization: `Bearer ${data[0].accessToken}`
           }
-        }).reduce(
-          (a, c, i) => (
-            a.filter(elem => elem.username === c.username).length === 0 ?
-            a.push(c) :
-            (a.find(_elem => _elem.username === c.username).amount += c.amount),
-            a
-          ),
-          []
-        ).sort((a, b) => b.amount - a.amount).slice(0, 100);
-        res.send(donations);
+        }).then(_data => {
+          result = [...result, ..._data.data.data];
+          if (_data.data.links.next) {
+            getDonation(`${DA_URL}/api/v1/alerts/donations?${(_data.data.links.next).split('?')[1]}`);
+          } else {
+            result = result.map(w => {
+              if (w.currency === "RUB") {
+                return {
+                  amount: w.amount,
+                  username: w.username
+                };
+              } else if (w.currency === "USD") {
+                return {
+                  amount: w.amount * 62,
+                  username: w.username
+                };
+              } else if (w.currency === "EUR") {
+                return {
+                  amount: w.amount * 71,
+                  username: w.username
+                };
+              }
+            }).reduce(
+              (a, c, i) => (
+                a.filter(elem => elem.username === c.username).length === 0 ?
+                a.push(c) :
+                (a.find(_elem => _elem.username === c.username).amount += c.amount),
+                a
+              ),
+              []
+            ).sort((a, b) => b.amount - a.amount).slice(0, 100);
+            res.send(result);
+          }
       }).catch(e => {
         axios.post(`${DA_URL}/oauth/token`, {
           grant_type: "refresh_token",
@@ -111,6 +119,8 @@ app.get("/getDonations/:user_id", (req, res) => {
           }).then(() => res.redirect(`${REDIRECT}/getDonations/${req.params.user_id}`));
         });
       });
+      }
+      getDonation(`${DA_URL}/api/v1/alerts/donations`);
     } else {
       res.send([]);
     }
